9bcc44a7d68d29739f29618c78b21312156e2c03
Alvaro <aelguedaicc@gmail.com>
2018-04-13 12:28:25 -0300

    Fixed
    changed extension .scss to .css and added imports

--- ClientApp/app/components/detalle1d/detalle1d.component.ts
+++ ClientApp/app/components/detalle1d/detalle1d.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detalle1d',
   templateUrl: './detalle1d.component.html',
-  styleUrls: ['./detalle1d.component.scss']
+  styleUrls: ['./detalle1d.component.css']
 })
 export class Detalle1dComponent implements OnInit {
 
