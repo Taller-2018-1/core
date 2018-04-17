f00659412d6a608494832ee7783c14afe8a2f8b5
Unknown <suflo.pedro@gmail.com>
2018-04-17 18:14:56 -0400

    fix(feat(detalle1B))
    -fix an error with navbar typing Detalle, now the navbar has Detalles Indicador 1B.
    -fix an error with navbar type header, now the navbar has <h2> on the title.
    - Change the folder for services to app then service, and solve the problems with the new rout of entry.

--- ClientApp/app/services/detail1b/detail1b.service.ts
+++ ClientApp/app/services/detail1b/detail1b.service.ts
@@ -2,15 +2,15 @@
 import { Injectable } from '@angular/core'; 
 import { Http, Response } from '@angular/http'; 
 import { Observable } from 'rxjs/Observable'; 
-import { Detail1b } from '../../detail1b/detail1b.component'; 
+import { Detail1b } from '../../components/detail1b/detail1b.component'; 
 import 'rxjs/add/operator/map';
-import { getBaseUrl } from '../../../app.browser.module'; 
+import { getBaseUrl } from '../../app.browser.module'; 
  
 @Injectable() 
 export class Detail1bService { 
  
   constructor(private http: Http) {     
-  } 
+  }
   
   getDetails(): Observable<Detail1b[]> {      
     return this.http.get(getBaseUrl() + 'api/Indicator1b/GetDetail') 
