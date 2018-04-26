b571f4bb8946a823cb62aea3d925506b2068fcd5
CorperK <corperk@localhost.localdomain>
2018-04-26 14:15:14 -0300

    Fix(Connection String)
    Change Localmachine name with `.` that indicated Localmachine to SQLServer
    
     Committer: ALisboa13 <AlanDeniLisboa@Gmail.com>

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -34,7 +34,7 @@ namespace think_agro_metrics.Data
             //comienzo  de la jerarquia en SMSS
             //El campo Database corresponde al nombre de  la base de datos a utilizar.
             //El otro ponganlo because of reasons.
-            optionsBuilder.UseSqlServer("Server=DESKTOP-RC34OJH\\SQLEXPRESS;Database=think_agro_metrics;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=think_agro_metrics;Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
