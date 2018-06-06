87607933adf5f97d6c94b86466b8748949cc3808
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2018-06-06 17:03:22 -0400

    feat(GlobalFilters): completed
    The filter for Year and  Month are now "global".
    I mean, the year and month selected by dropdowns in indicator-display will be automatically selected on indicator-detail (and vice versa).
    Also, the data, value and golas are selected according to the filters.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -32,6 +32,8 @@ import { IndicatorDetailRegistryComponent } from './components/indicator-detail/
 import { AuthService } from './services/auth/AuthService';
 import { CanActivateUser } from './services/auth/CanActivateService';
 import { WelcomeComponent } from './components/welcome-component/welcome-component.component';
+import { StorageServiceModule } from 'ngx-webstorage-service';
+import { SessionService } from './services/session/session.service';
 
 @NgModule({
   declarations: [
@@ -63,6 +65,7 @@ import { WelcomeComponent } from './components/welcome-component/welcome-compone
     TabsModule.forRoot(),
     AccordionModule.forRoot(),
     FlexLayoutModule,
+    StorageServiceModule ,
 
     RouterModule.forRoot([
       { path: 'indicator/:idIndicatorGroup/:idIndicator', component: IndicatorDetailComponent },
@@ -74,7 +77,7 @@ import { WelcomeComponent } from './components/welcome-component/welcome-compone
       { path: '**',          component: ResultHomeComponent },
     ])
   ],
-  providers: [IndicatorService, IndicatorGroupService, RegistryService, AuthService, CanActivateUser],
+  providers: [IndicatorService, IndicatorGroupService, RegistryService, AuthService, CanActivateUser, SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
