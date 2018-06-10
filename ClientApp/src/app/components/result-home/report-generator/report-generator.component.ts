4e826aa41d8f013d8efe718b499c915df82d94d4
Rodolfo <rhenzi13@alumnos.utalca.cl>
2018-06-10 02:58:13 -0400

    Feature(PDFGenerator): I add librery to generate PDF

--- ClientApp/src/app/components/result-home/report-generator/report-generator.component.ts
+++ ClientApp/src/app/components/result-home/report-generator/report-generator.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-report-generator',
@@ -6,6 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-generator.component.css']
 })
 export class ReportGeneratorComponent implements OnInit {
+
+  @ViewChild('content') content: ElementRef;
+  public downloadPDF()
+  {
+      let doc = new jsPDF();
+
+      let specialElementHandlers = {
+          '#editor': function(element, renderer){
+            return true;
+          }
+      };
+
+      let content = this.content.nativeElement;
+
+      doc.fromHTML(content.innerHTML, 15, 15, {
+        'with':190,
+        'elementHandlers':specialElementHandlers
+      });
+
+      doc.save('test.pdf');
+
+  }
+
+  
 
   constructor() { }
 
