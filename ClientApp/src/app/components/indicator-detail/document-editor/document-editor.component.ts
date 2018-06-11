4ab12e83cae5c6ae6792bcfafc4c4d7d5a7373cb
Juan Ignacio <jcorderuiz@gmail.com>
2018-06-11 17:56:12 -0400

    fix(editModalDocument): not completed

--- ClientApp/src/app/components/indicator-detail/document-editor/document-editor.component.ts
+++ ClientApp/src/app/components/indicator-detail/document-editor/document-editor.component.ts
@@ -6,6 +6,7 @@ import { FileService } from '../../../services/file/file.service';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 import { Document } from '../../../shared/models/document';
+import { equal, deepEqual, notDeepEqual } from 'assert';
 
 @Component({
   selector: 'app-document-editor',
@@ -16,6 +17,10 @@ export class DocumentEditorComponent implements OnInit {
 
   @Input()
   public document: Document;
+  public newDocument: Document;
+
+  @Input()
+  public documents: Document[];
 
   @Input()
   public editModalRef: BsModalRef;
@@ -24,10 +29,23 @@ export class DocumentEditorComponent implements OnInit {
 
   constructor(private service: FileService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.newDocument= JSON.parse(JSON.stringify(this.document)); // To create a clone of the selected document (this.document)
+  }
 
   editDocument() {
-    this.service.editDocument(this.document).subscribe();
+    try {
+      notDeepEqual(this.document, this.newDocument); // If document and newDcoument are not equal, just close the modal 
+      this.service.editDocument(this.newDocument).subscribe();
+      // replacing the old registry (this.document) with the edited document (this.newDocument) 
+      let index = this.documents.indexOf(this.document);
+      this.documents[index] = this.newDocument;
+
+    }
+    catch (error) {
+
+    }
+
     this.editModalRef.hide();
     // this.document = null;
     this.editModalRef = null;
