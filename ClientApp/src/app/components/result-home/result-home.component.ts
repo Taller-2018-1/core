b49b323b1b0343edfe3711726c7722cd7a169070
Daniel Pavez <kirito.furukawa@gmail.com>
2018-07-30 01:12:51 -0400

    Fix Merge remote-tracking branch 'origin/Feature/ChangeButton' into DateFilter

--- ClientApp/src/app/components/result-home/result-home.component.ts
+++ ClientApp/src/app/components/result-home/result-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding,TemplateRef } from '@angular/core';
+import { Component, OnInit, HostBinding, TemplateRef } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
 
@@ -12,7 +12,7 @@ import { IndicatorGroupService } from '../../services/indicator-group/indicator-
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
-//Importa libreria PDF
+// Importa libreria PDF
 import * as jsPDF from 'jspdf';
 import { TemplateAst } from '@angular/compiler';
 
@@ -27,9 +27,7 @@ export class ResultHomeComponent implements OnInit {
   @HostBinding('class') classes = 'wrapper'; // This adds a class to the host container
 
   public indicatorGroups$: Observable<IndicatorGroup[]>;
-  public indicatorGroupsComplete$ : Observable<IndicatorGroup[]>;
-
-  public indicatorGroupsComplete$ : Observable<IndicatorGroup[]>; 
+  public indicatorGroupsComplete$: Observable<IndicatorGroup[]>;
 
   modalRef: BsModalRef;
 
@@ -37,23 +35,17 @@ export class ResultHomeComponent implements OnInit {
 
   }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.indicatorGroups$ = this.service.getIndicatorGroups();
     this.indicatorGroupsComplete$ = this.service.getIndicatorGroupsComplete();
   }
 
-    this.indicatorGroupsComplete$ = this.service.getIndicatorGroupsComplete(); 
-  }
 
-
-  openModalAdd(template: TemplateRef<any>)
-  {
+  openModalAdd(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
-  indicatorGroupAdded()
-  {
+  indicatorGroupAdded() {
     this.indicatorGroups$ = this.service.getIndicatorGroups();
   }
 
