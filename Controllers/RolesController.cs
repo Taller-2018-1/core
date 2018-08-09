1b92f93f5ebc1086ee3ed201f33ea787e8783a1d
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2018-08-08 22:23:22 -0400

    TestRolesApi

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using think_agro_metrics.Data;
+using think_agro_metrics.Models;
+
+namespace think_agro_metrics.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Roles")]
+    public class RolesController : Controller
+    {
+        private readonly DataContext _context;
+
+
+        public RolesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Roles
+        [HttpGet]
+        public async Task<IActionResult> GetRoles()
+        {
+            var roles = await _context.Roles.ToListAsync();
+            return Ok(roles);
+        }
+
+        // GET: api/Roles/Complete
+        [HttpGet("Complete")]
+        public async Task<IActionResult> GetRolesComplete()
+        {
+            var roles = await _context.Roles
+                .Include(r => r.PermissionsRead)
+                .Include(r => r.PermissionsWrite).ToListAsync();
+            return Ok(roles);
+        }
+
+        // POST api/Roles/5/Permission/Read // To update or add permissions
+        [HttpPost("{id}/Permissionn/Read")]
+        public async Task<IActionResult> AddPermissionRead([FromRoute] long id, [FromBody] Indicator indicator)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var role = await _context.Roles.SingleAsync(r => r.RoleID == id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            role.PermissionsRead.Add(indicator);
+            _context.Entry(role).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(role);
+
+        }
+
+        // POST api/Roles/5/Permission/Read
+        [HttpPut("{id}/Permission/Write")]
+        public async Task<IActionResult> AddPermissionWrite([FromRoute] long id, [FromBody] Indicator indicator)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var role = await _context.Roles.SingleAsync(r => r.RoleID == id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            role.PermissionsWrite.Add(indicator);
+            _context.Entry(role).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(role);
+        }
+    }
+}
---
+++ Models/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace think_agro_metrics.Models
+{
+    public class Role
+    {
+        public long RoleID { get; set; }
+        public string RoleName { get; set; }
+        public string RoleToken { get; set; }
+        public ICollection<Indicator> PermissionsRead { get; set; }
+        public ICollection<Indicator> PermissionsWrite { get; set; }
+
+        public Role()
+        {
+            this.PermissionsRead = new List<Indicator>();
+            this.PermissionsWrite = new List<Indicator>();
+        }
+    }
+}
